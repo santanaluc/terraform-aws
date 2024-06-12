cd540bb3e85abb42de4959ce86664c9541785918
Lucas Santana <lucas.s.s.silva@hotmail.com>
2024-06-12 16:43:38 -0300

    feat: using terraform lint

--- provider.tf
+++ provider.tf
@@ -1,13 +1,13 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.53.0"
     }
   }
   backend "s3" {
     bucket = "terraform-to-aws"
-    key = "dev/terraform.tfstate"
+    key    = "dev/terraform.tfstate"
     region = "us-east-1"
   }
 }
