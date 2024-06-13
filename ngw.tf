4995e860a4dbf8bfad7b92ef672eddfe3db83bf4
Lucas Santana <lucas.s.s.silva@hotmail.com>
2024-06-13 13:27:13 -0300

    feat: add nat gateway to private subnet

--- ngw.tf
+++ ngw.tf
@@ -18,3 +18,26 @@ resource "aws_eip" "eks_ngw_eip_1b" {
   )
 }
 
+resource "aws_nat_gateway" "eks_ngw_1a" {
+  allocation_id = aws_eip.eks_ngw_eip_1a.id
+  subnet_id     = aws_subnet.eks_subnet_public_1a.id
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-ngw-1a"
+    }
+  )
+}
+
+resource "aws_nat_gateway" "eks_ngw_1b" {
+  allocation_id = aws_eip.eks_ngw_eip_1b.id
+  subnet_id     = aws_subnet.eks_subnet_public_1b.id
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-ngw-1b"
+    }
+  )
+}
