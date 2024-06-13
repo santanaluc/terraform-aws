3fc90d714b46078192ef58f8d5660b9ecb1476e0
Lucas Santana <lucas.s.s.silva@hotmail.com>
2024-06-13 14:11:06 -0300

    feat: creating output subnets

---
+++ modules/network/output.tf
@@ -0,0 +1,11 @@
+output "subnet_public_1a" {
+  value = aws_subnet.eks_subnet_public_1a.id
+}
+
+output "subnet_public_1b" {
+  value = aws_subnet.eks_subnet_public_1b.id
+}
+
+output "subnet_private_1b" {
+  value = aws_subnet.eks_subnet_public_1b.id
+}
