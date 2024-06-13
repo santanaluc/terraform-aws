830c45ec89b1b313459a74f60f2632a3d700a8d6
Lucas Santana <lucas.s.s.silva@hotmail.com>
2024-06-13 19:44:14 -0300

    feat: creating managed node groups

---
+++ modules/mng/variables.tf
@@ -0,0 +1,24 @@
+variable "project_name" {
+  type        = string
+  description = "Project name to be used to name the resources"
+}
+
+variable "tags" {
+  type        = map(any)
+  description = "Tags to be added"
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "Cluster name"
+}
+
+variable "subnet_private_1a" {
+  type        = string
+  description = "Subnet ID from AZ 1a"
+}
+
+variable "subnet_private_1b" {
+  type        = string
+  description = "Subnet ID from AZ 1a"
+}
